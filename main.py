6d108830ea82eb01f42b957396ecb12376af61dd
princesamee <81880+princesamee@users.noreply.github.com>
2026-06-27 23:40:55 -0300

    Suma puntaje placa vs humano

--- main.py
+++ main.py
@@ -1,11 +1,69 @@
-hand = 0
+
 def on_gesture_shake():
+    limpiar_matriz()
+    dibujar_jugada()
+    esperar_boton()
+    music._play_default_background(music.built_in_playable_melody(Melodies.DADADADUM), music.PlaybackMode.IN_BACKGROUND)
+    sumar_puntaje()
+
+def mostrar_puntaje(jugador, numero):
+    if numero == 0:
+        basic.show_string("Empate")
+    else:
+        basic.show_string(jugador)
+        basic.show_number(numero)
+
+def esperar_boton():
+    jugador_a = input.button_is_pressed(Button.A)
+    jugador_b = input.button_is_pressed(Button.B)
+        
+    while not (jugador_a or jugador_b):
+        jugador_a = input.button_is_pressed(Button.A)
+        jugador_b = input.button_is_pressed(Button.B)
+
+
+
+    
+def dibujar_jugada():    
     global hand
     hand = randint(1,3)
+    
     if hand == 1:
-        basic.show_icon(IconNames.SMALL_SQUARE)
+        #piedra
+        dibujar_cuadrado()
     elif hand == 2:
+        #papel
         basic.show_icon(IconNames.SQUARE)
     else:
+        #Tijera
         basic.show_icon(IconNames.SCISSORS)
+
+def dibujar_cuadrado():
+    for x in range (1,4):
+        for y in range(1,4):
+            led.plot(x, y)
+
+def limpiar_matriz():
+    for x in range (0,5):
+            for y in range(0,5):
+                led.unplot(x, y)
+
+def sumar_puntaje():
+    jugador_a = input.button_is_pressed(Button.A)
+    jugador_b = input.button_is_pressed(Button.B)
+    empate = jugador_a and jugador_b
+    if empate:
+        mostrar_puntaje("",0)
+    elif jugador_a:
+        global jugador
+        jugador += 1
+        mostrar_puntaje("Hum", jugador)
+    elif jugador_b:
+        global microbit
+        microbit += 1
+        mostrar_puntaje("AI", microbit)
+
+hand = 0 # inicio sin nada
+jugador = 0
+microbit = 0
 input.on_gesture(Gesture.SHAKE, on_gesture_shake)
--- main.ts
+++ main.ts
@@ -1,13 +1,80 @@
-let hand = 0
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
+function mostrar_puntaje(jugador: string, numero: number) {
+    if (numero == 0) {
+        basic.showString("Empate")
+    } else {
+        basic.showString(jugador)
+        basic.showNumber(numero)
+    }
+    
+}
+
+function esperar_boton() {
+    let jugador_a = input.buttonIsPressed(Button.A)
+    let jugador_b = input.buttonIsPressed(Button.B)
+    while (!(jugador_a || jugador_b)) {
+        jugador_a = input.buttonIsPressed(Button.A)
+        jugador_b = input.buttonIsPressed(Button.B)
+    }
+}
+
+function dibujar_jugada() {
     
     hand = randint(1, 3)
     if (hand == 1) {
-        basic.showIcon(IconNames.SmallSquare)
+        // piedra
+        dibujar_cuadrado()
     } else if (hand == 2) {
+        // papel
         basic.showIcon(IconNames.Square)
     } else {
+        // Tijera
         basic.showIcon(IconNames.Scissors)
     }
     
+}
+
+function dibujar_cuadrado() {
+    for (let x = 1; x < 4; x++) {
+        for (let y = 1; y < 4; y++) {
+            led.plot(x, y)
+        }
+    }
+}
+
+function limpiar_matriz() {
+    for (let x = 0; x < 5; x++) {
+        for (let y = 0; y < 5; y++) {
+            led.unplot(x, y)
+        }
+    }
+}
+
+function sumar_puntaje() {
+    let jugador_a = input.buttonIsPressed(Button.A)
+    let jugador_b = input.buttonIsPressed(Button.B)
+    let empate = jugador_a && jugador_b
+    if (empate) {
+        mostrar_puntaje("", 0)
+    } else if (jugador_a) {
+        
+        jugador += 1
+        mostrar_puntaje("Hum", jugador)
+    } else if (jugador_b) {
+        
+        microbit += 1
+        mostrar_puntaje("AI", microbit)
+    }
+    
+}
+
+let hand = 0
+//  inicio sin nada
+let jugador = 0
+let microbit = 0
+input.onGesture(Gesture.Shake, function on_gesture_shake() {
+    limpiar_matriz()
+    dibujar_jugada()
+    esperar_boton()
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.InBackground)
+    sumar_puntaje()
 })
